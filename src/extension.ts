cd96397252a15ce8becd69d8c8e17a8c3b715c1f
Kidd911 <138651628+Kidd911@users.noreply.github.com>
2024-08-01 12:24:40 -0600

    Initialize patch-1

---
+++ src/extension.ts
@@ -0,0 +1,110 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import { FpcItem, FpcProjectProvider } from './providers/project';
+import { diagCollection, FpcTaskProvider,taskProvider } from './providers/task';
+import { FpcCommandManager } from './commands';
+import * as util from './common/util';
+import {TLangClient} from './languageServer/client';
+import { configuration } from './common/configuration';
+import { JediFormatter } from './formatter';
+import { format } from 'path';
+import * as MyCodeAction from  './languageServer/codeaction';
+export let client:TLangClient;
+export let formatter:JediFormatter;
+export let logger:vscode.OutputChannel;
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export async function activate(context: vscode.ExtensionContext) {
+
+	if (!vscode.workspace.workspaceFolders) {
+		return;
+	}
+
+	logger=vscode.window.createOutputChannel('fpctoolkit');
+
+	vscode.window.onDidChangeVisibleTextEditors(onDidChangeVisibleTextEditors);
+	vscode.workspace.onDidChangeTextDocument(onDidChangeTextDocument);
+	util.setExtensionContext(context);
+	const workspaceRoot = vscode.workspace.workspaceFolders[0].uri.fsPath;
+
+
+	let commands = new FpcCommandManager(workspaceRoot);
+	commands.registerAll(context);
+
+	formatter=new JediFormatter();
+	formatter.doInit();
+	
+	let ProjectProvider= new FpcProjectProvider(workspaceRoot,context);
+	vscode.window.registerTreeDataProvider("FpcProjectExplorer", ProjectProvider);
+
+	//taskProvider=new FpcTaskProvider(workspaceRoot);
+	context.subscriptions.push(vscode.tasks.registerTaskProvider(
+		FpcTaskProvider.FpcTaskType,
+		taskProvider
+	)
+	);
+
+	
+
+	MyCodeAction.activate(context);
+
+	client=new TLangClient(ProjectProvider);
+	await client.doInit();
+	client.start();
+
+
+}
+
+function onDidChangeTextDocument(e:vscode.TextDocumentChangeEvent){
+	if(e.contentChanges.length>0){
+		if(!diagCollection.has(e.document.uri)){ return ;}
+		
+		
+		for (const change of e.contentChanges) {
+			let newline=(change.text.match(/\n/g) || '').length+1;
+			if(change.range.isSingleLine && (newline<2)){
+				continue;
+			}
+			let diags= diagCollection.get(e.document.uri);
+			if(!diags){return;}
+
+			let oldline=change.range.end.line-change.range.start.line+1;
+			
+			let lines_change=newline-oldline;
+			let newdiags=[];
+			for (const diag of diags) {
+				if(change.range.contains(diag.range)){//remove it if contains
+					continue;
+				}
+			
+				if(diag.range.start.line>=change.range.start.line){
+					diag.range=new vscode.Range(diag.range.start.line+lines_change,diag.range.start.character,diag.range.end.line+lines_change,diag.range.end.character)
+				}
+				newdiags.push(diag)
+			}
+			diagCollection.set(e.document.uri, newdiags);
+		}
+		
+		
+		
+	}
+	
+}
+function onDidChangeVisibleTextEditors(editors: readonly vscode.TextEditor[]): void {
+    // Process delayed didOpen for any visible editors we haven't seen before
+    editors.forEach(editor => {
+        if ((editor.document.uri.scheme === "file") && (editor.document.languageId === "objectpascal" || editor.document.languageId === "pascal" )) {
+			editor.options.tabSize=configuration.get<number>('format.tabsize',2);
+			client.onDidChangeVisibleTextEditor(editor);
+        }
+    });
+}
+// this method is called when your extension is deactivated
+export function deactivate() {
+	if (!client) {
+		return undefined;
+	}
+	return client.stop();
+}
---
+++ src/formatter.ts
@@ -0,0 +1,101 @@
+import * as vscode from 'vscode';
+import * as ChildProcess from "child_process";
+import * as util from './common/util';
+import path = require('path');
+import { configuration } from './common/configuration';
+import { env } from 'process';
+import * as fs from 'fs';
+import { client, logger } from './extension';
+import { Console } from 'console';
+
+export class JediFormatter {
+    private jcfpath:string;
+    private default_cfg:string;
+    private is_win:boolean=true;
+    constructor() {
+        const plat: NodeJS.Platform = process.platform;
+        const arch = process.arch;
+        let extensionProcessName = '';
+    
+        if (arch === 'x64') {
+            if (plat === 'win32') {
+                extensionProcessName = 'win32/jcf.exe';
+            } else if (plat === 'linux') {
+                extensionProcessName = 'x86_64-linux/jcf';
+            } else if (plat == 'darwin') {
+                extensionProcessName = 'x86_64-darwin/jcf';
+            }
+            else {
+                throw "Invalid Platform";
+            }
+        } else if (arch === 'arm64') {
+            if (plat === 'linux') {
+                extensionProcessName = 'aarch64-linux/jcf';
+            } else if (plat == 'darwin') {
+                extensionProcessName = 'x86_64-darwin/jcf';
+            }
+            else if (plat == 'win32') {
+                extensionProcessName = 'win32/jcf.exe';
+            } else {
+                throw "Invalid Platform";
+            }
+        } else {
+            throw "Invalid arch";
+        }
+        this.is_win=plat==='win32';
+     
+        this.jcfpath = path.resolve(util.getExtensionFilePath("bin"), extensionProcessName);
+        if(!this.is_win){
+            fs.chmodSync(this.jcfpath,755);
+        }
+        this.default_cfg=path.resolve(util.getExtensionFilePath("bin"),'jcfsettings.cfg');
+        let cfg_path='';
+        if(this.is_win){
+            cfg_path=env.LOCALAPPDATA+'/lazarus/jcfsettings.cfg'; 
+        }else{
+            cfg_path=env.HOME+'/.lazarus/jcfsettings.cfg';
+        };
+        if(fs.existsSync(cfg_path)){
+            this.default_cfg=cfg_path;
+        }
+        
+
+    }
+    getCfgConfig():string{
+        let cfg=configuration.get<string>("format.cfgpath","");
+        if(cfg==""){
+            cfg=this.default_cfg;
+        }
+        return cfg;
+    }
+    doInit() {
+        let _this = this;
+        let enable=configuration.get<boolean>('format.enabled',true);
+        if(!enable) return;
+        // 👍 formatter implemented using API
+        vscode.languages.registerDocumentFormattingEditProvider('objectpascal', {
+            provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.ProviderResult<vscode.TextEdit[]> {
+                let cfg_path=_this.getCfgConfig();
+                let proc= ChildProcess.spawn(_this.jcfpath, ['-inplace', '-y','-config='+cfg_path,'-F' ,document.fileName] );
+                logger.appendLine(proc.spawnargs.join(' '));
+                proc.stdout.on('data', (data) => {
+                    logger.appendLine(data);
+                    console.log(`stdout: ${data}`);
+                });
+
+                proc.stderr.on('data', (data) => {
+                    logger.appendLine(data);
+                    console.error(`stderr: ${data}`);
+                });
+
+                proc.on('close', (code) => {
+                    console.log(`child process exited with code ${code}`);
+                });
+                proc.unref();
+                return [];
+            }
+        });
+
+    }
+}
+
